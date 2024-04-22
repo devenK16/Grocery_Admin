e3f32ab3c8305aaa595de3021bf77ee8a1212c3c
devenK16 <devenkhobragade007@gmail.com>
2024-04-22 15:49:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/grocery_admin/auth/SplashFragment.kt
@@ -0,0 +1,28 @@
+package com.example.grocery_admin.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.grocery_admin.ARG_PARAM1
+import com.example.grocery_admin.ARG_PARAM2
+import com.example.grocery_admin.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SplashFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SplashFragment : Fragment() {
+    private lateinit var binding: FragmentSplashBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+}
